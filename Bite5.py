288e215bedc437cbf1c4984610d7472a5a9b706d
makgunay <makgunay@gmail.com>
2018-01-19 13:16:57 +0400

    Initial commit

---
+++ Bite5.py
@@ -0,0 +1,19 @@
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+
+def dedup_and_title_case_names(names):
+    """Should return a list of names, each name appears only once"""
+    pass
+
+
+def sort_by_surname_desc(names):
+    names = dedup_and_title_case_names(names)
+    # ...
+
+
+def shortest_first_name(names):
+    names = dedup_and_title_case_names(names)
+    # ...
