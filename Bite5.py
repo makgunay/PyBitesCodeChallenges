793f3e16719290074e4fc03eff22c16dbd048c21
makgunay <makgunay@gmail.com>
2018-01-22 00:17:36 +0400

    Challange completed

--- Bite5.py
+++ Bite5.py
@@ -3,8 +3,6 @@ NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
          'julbob pybites', 'bob belderbos', 'julian sequeira',
          'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
 
-
-
 def dedup_and_title_case_names(names):
     """Should return a list of names, each name appears only once"""
     NAMES2 = []
@@ -15,15 +13,25 @@ def dedup_and_title_case_names(names):
             NAMES2.append(item.title())
     return NAMES2
 
-print(dedup_and_title_case_names(NAMES))
-
 def sort_by_surname_desc(names):
+    NAMES3 = []
+    NAMES4 = []
     names = dedup_and_title_case_names(names)
-
-
-    # ...
-
+    for item in names:
+        NAMES3.append(item.split())
+        NAMES3 = sorted(NAMES3, key=lambda x: x[1], reverse=True)
+        NAMES4 = [x+" "+y for x,y in NAMES3]
+    return NAMES4
 
 def shortest_first_name(names):
     names = dedup_and_title_case_names(names)
-    # ...
+    NAMES5 = []
+    for item in names:
+        NAMES5.append(item.split())
+        NAMES5 = sorted(NAMES5, key=lambda x: x[0])
+    return NAMES5[0][0]
+
+
+print(dedup_and_title_case_names(NAMES))
+print(sort_by_surname_desc(NAMES))
+print(shortest_first_name(NAMES))
